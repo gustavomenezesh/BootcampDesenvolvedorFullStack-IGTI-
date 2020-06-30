6fd5f9d490802b7f5e4da6c1ccefbefb2e89a9a5
gustavomenezesh <45804139+gustavomenezesh@users.noreply.github.com>
2020-06-29 23:02:44 -0300

    Saque, saldo, transferencia e remoção

--- 4-BancoDeDadosEVersionamento/src/Controllers/accountController.js
+++ 4-BancoDeDadosEVersionamento/src/Controllers/accountController.js
@@ -31,6 +31,114 @@ module.exports = {
         }else
             res.send({err: "Account not found!"});
         
+    },
+
+    async draw(req, res){
+
+        const {agencia, conta, value} = req.body;
+
+        const account = await Account.findOne({agencia: agencia, conta: conta});
+
+        if(account){
+
+            if(account.balance >= value){
+
+                const newVal = account.balance - value;
+                const accountUpdated = await Account.updateOne(
+                    {agencia: agencia, conta: conta},
+                    {$set: {balance: newVal}}
+                )
+
+                res.send({msg: 'Sucessful Operation'})
+
+            }else
+                res.send({msg:'Balance insuficient'});
+
+        }else  
+            res.send({msg:'Account not found'});
+
+    },
+
+    async consultBalance(req, res){
+
+        const {agencia, conta} = req.body;
+
+        const account = await Account.findOne({agencia: agencia, conta: conta});
+
+        account ? res.send({balance: account.balance}) : res.send({err: 'Account not found'});
+
+    },
+
+    async deleteAccount(req, res){
+        const {agencia, conta} = req.body;
+
+        const account = await Account.deleteOne({agencia: agencia, conta: conta});
+
+        if(account.deletedCount){
+            const accounts = await Account.find({agencia: agencia});
+            res.send({accountsCount: accounts.length});
+        }else
+            res.send({err: 'Account not found'});
+
+    },
+
+    async transfer(req, res){
+
+        const {origin, distiny, value} = req.body;
+
+        const accountDistiny = await Account.findOne({conta: distiny});
+        const accountOrigin = await Account.findOne({conta: origin});
+
+        if(accountDistiny && accountOrigin){
+
+            if(accountOrigin.agencia === accountDistiny.agencia){
+
+                if(accountOrigin.balance >= value){
+
+                    const originBalance = accountOrigin.balance - value;
+                    const destinyBalance = accountDistiny.balance + value;
+
+                    const originUpdated = await Account.updateOne(
+                        {agencia: accountOrigin.agencia, conta: accountOrigin.conta}, 
+                        {$set: {balance: originBalance}}
+                    );
+
+                    const destinyUpdated = await Account.updateOne(
+                        {agencia: accountDistiny.agencia, conta: accountDistiny.conta},
+                        {$set: {balance: destinyBalance}}
+                    );
+
+                    res.send({balance: originBalance});
+
+                }else
+                    res.send({err: 'Balance insuficient'})
+            }else{
+
+                if(accountOrigin.balance >= value+8){
+
+                    const originBalance = accountOrigin.balance - value+8;
+                    const destinyBalance = accountDistiny.balance + value;
+
+                    const originUpdated = await Account.updateOne(
+                        {agencia: accountOrigin.agencia, conta: accountOrigin.conta}, 
+                        {$set: {balance: originBalance}}
+                    );
+
+                    const destinyUpdated = await Account.updateOne(
+                        {agencia: accountDistiny.agencia, conta: accountDistiny.conta},
+                        {$set: {balance: destinyBalance}}
+                    );
+
+                    res.send({balance: originBalance});
+
+                }else
+                    res.send({err: 'Balance insuficient'})
+
+            }
+
+        }else
+            res.send({err: 'Account distiny or origin not found'});
+
     }
 
 
--- 4-BancoDeDadosEVersionamento/src/routes.js
+++ 4-BancoDeDadosEVersionamento/src/routes.js
@@ -5,5 +5,11 @@ const routes = Router();
 
 routes.post('/', accountController.init);
 routes.post('/deposit', accountController.deposit);
+routes.post('/draw', accountController.draw);
+routes.post('/transfer', accountController.transfer);
+
+routes.get('/balance', accountController.consultBalance);
+
+routes.delete('/delete', accountController.deleteAccount);
 
 module.exports = routes;
